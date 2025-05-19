ecbc5032ff5fa00228f6ab1bc2b214b328df2b17
TeEHuii <amares.babe@gmail.com>
2025-05-19 15:05:56 +0700

    Build 1.5.0

---
+++ docs/assets/js/acecf23e.964b6317.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/B2C-Manual/blog","blogTitle":"Blog","authorsListPath":"/B2C-Manual/blog/authors"}')}}]);
